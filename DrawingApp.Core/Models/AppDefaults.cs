8d5717bb0a89958f461dbd07d183372db863af60
22120358 <phamchithuan777@gmail.com>
2025-12-05 22:30:17 +0700

    feat(core): add AppDefaults model

---
+++ DrawingApp.Core/Models/AppDefaults.cs
@@ -0,0 +1,8 @@
+﻿namespace DrawingApp.Core.Models;
+
+public static class AppDefaults
+{
+    public const double DefaultBoardWidth = 900;
+    public const double DefaultBoardHeight = 600;
+    public const string DefaultBoardBg = "#FFFFFFFF";
+}
